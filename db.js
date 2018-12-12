9d47be64b758a9ff4e272b0ebdfbbd1cf81ece2d
Jacob <jaa134@case.edu>
2018-12-12 16:05:15 -0500

    stops problems with server hanging on deploy

--- db.js
+++ db.js
@@ -3,52 +3,55 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
-if (!process.env.DATABASE_URL) {
+module.exports = {
+    resetConnection: resetConnection,
+    getAllLocations: getAllLocations,
+    updateLocation: updateLocation
+};
+
+var conString = process.env.DATABASE_URL;
+if (!conString) {
     console.log("Local db connection string not set!")
     process.exit(-1);
 }
 else
-    console.log("Connection String: " + process.env.DATABASE_URL);
+    console.log("Connection String: " + conString);
 
-var conString = process.env.DATABASE_URL;
 var client = new pg.Client(conString);
-client.connect(function(err) {
-    if(err) {
-        console.error('Could not connect to postgres...', err);
-        process.exit(-1);
-    }
 
-    const queryText =
-    `
-    DROP TABLE IF EXISTS business;
-
-    CREATE TABLE IF NOT EXISTS
-        business(
-            location_name VARCHAR(128) PRIMARY KEY,
-            location_type INTEGER NOT NULL,
-            extent INTEGER NOT NULL,
-            last_updated TIMESTAMP
-        );
-
-    INSERT INTO business(location_name, location_type, extent, last_updated)
-    VALUES 
-        ('Home', 0, 0, NOW());
-    `;
-    client.query(queryText, function(err, result) {
+function resetConnection() {
+    client.connect(function(err) {
         if(err) {
-            console.error('Error running create table query...', err);
+            console.error('Could not connect to postgres...', err);
             process.exit(-1);
         }
-
-        console.log(result);
-        //client.end();
+    
+        const queryText =
+        `
+        DROP TABLE IF EXISTS business;
+    
+        CREATE TABLE IF NOT EXISTS
+            business(
+                location_name VARCHAR(128) PRIMARY KEY,
+                location_type INTEGER NOT NULL,
+                extent INTEGER NOT NULL,
+                last_updated TIMESTAMP
+            );
+    
+        INSERT INTO business(location_name, location_type, extent, last_updated)
+        VALUES 
+            ('Home', 0, 0, NOW());
+        `;
+        client.query(queryText, function(err, result) {
+            if(err) {
+                console.error('Error running create table query...', err);
+                process.exit(-1);
+            }
+    
+            console.log(result);
+        });
     });
-});
-
-module.exports = {
-    getAllLocations: getAllLocations,
-    updateLocation: updateLocation
-};
+}
 
 function getAllLocations(cb) {
     client.query('select * from business', function(err, result) {
@@ -62,6 +65,6 @@ function getAllLocations(cb) {
     });
 }
 
-function updateLocation(req, res, next) {
+function updateLocation() {
 }
   
